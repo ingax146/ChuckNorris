77b0df827cc73c697fd6da6fd70e53eaca7de4a8
Ingemar Axelsson <ingemar.axelsson@gmail.com>
2024-10-26 18:56:32 +0200

    Initial Commit

---
+++ ChuckNorris/ChuckNorrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChuckNorrisApp.swift
+//  ChuckNorris
+//
+//  Created by Ingemar Axelsson on 2024-10-26.
+//
+
+import SwiftUI
+
+@main
+struct ChuckNorrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChuckNorrisTests/ChuckNorrisTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChuckNorrisTests.swift
+//  ChuckNorrisTests
+//
+//  Created by Ingemar Axelsson on 2024-10-26.
+//
+
+import Testing
+@testable import ChuckNorris
+
+struct ChuckNorrisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
